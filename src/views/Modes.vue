ab02400fe71747b52cd8e419986c2373a9bf98c2
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2021-01-31 21:54:01 +0100

    added base components

--- src/views/Modes.vue
+++ src/views/Modes.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="modes">
     <h1>Modes</h1>
+    <GameMode v-for="gameMode in allGameModes" :key="gameMode"></GameMode>
   </div>
 </template>
+
+<script>
+import GameMode from "@/components/GameMode.vue";
+export default {
+    name: "Modes",
+    components: {
+        GameMode,
+    },
+    computed: {
+        allGameModes () {
+            return this.$store.getters.allGameModes
+        }
+    }
+}
+</script>
