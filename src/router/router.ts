1f4e640423488fd1209134aca78b3613ca204012
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2023-03-23 21:06:24 +0100

    add routing

---
+++ src/router/router.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Overview from '@/views/Overview.vue'
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Overview,
+    },
+    {
+      path: '/game',
+      component: () => import('@/views/Game.vue'),
+    },
+    {
+      path: '/history',
+      component: () => import('@/views/History.vue'),
+    },
+  ],
+})
