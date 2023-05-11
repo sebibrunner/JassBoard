cc009cefa0788f18fb6a911d211330dcefc8e01e
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2023-05-11 23:43:38 +0200

    update tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      vue: 'vue/dist/vue.esm-bundler.js',
-      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
-      'balm-ui-css': 'balm-ui/dist/balm-ui.css',
     },
   },
   build: {
