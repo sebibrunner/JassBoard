127a6a4102723c4b7594f2eff770ab7487265ff4
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2023-05-11 23:32:56 +0200

    remove balmui

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 import router from './router/router'
 import { createPinia } from 'pinia'
-
-import BalmUI from 'balm-ui'; // Official Google Material Components
-import 'balm-ui-css';
 
 const app = createApp(App)
 const store = createPinia()
 app.use(router)
 app.use(store)
-app.use(BalmUI, {
-    UiTextfield: {
-
-    },
-    UiList: {
-
-    },
-    UiGrid: {
-        
-    }
-});
 app.mount('#app')
